6ee1435143767a1ce2fd1cb06ec2d796651ad0c6
romariclegrand <romaricyelkouni1@gmail.com>
2026-05-18 18:03:08 +0000

    Initialisation du projet Biblio_UNZ
    - Structure complète des dossiers apps/
    - Configuration Django (settings.py, urls.py)
    - Template parent base.html
    - Fichiers de base pour toutes les applications
    - Environnement Python configuré

---
+++ bibliotheque/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('apps.users.urls')),
+    path('books/', include('apps.books.urls')),
+    path('loans/', include('apps.loans.urls')),
+    path('notifications/', include('apps.notifications.urls')),
+    path('statistics/', include('apps.statistics.urls')),
+    path('', include('apps.users.urls')),  # Redirection vers login
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
